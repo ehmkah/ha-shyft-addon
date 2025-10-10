101f933b3c39516aa0cda9e67101562109ee4327
Michael Krausse <michael.krausse@directbox.com>
2025-10-10 16:09:06 +0200

    testing storing configuration

--- shyft-addon/app.py
+++ shyft-addon/app.py
@@ -2,15 +2,32 @@ import os
 from flask import Flask, send_from_directory, jsonify
 import requests
 import json
+import datetime
 
 app = Flask(__name__, static_folder="www", static_url_path="")
 SHYFT_ACCESS_KEY = "not_set_yet"
 OPTIONS_PATH = "/data/options.json"
+CONFIG_PATH = "/data/config.json"
 
 # Serve the static HTML
 @app.route("/")
 def index():
     return send_from_directory("www", "index.html")
+
+# config
+#{
+#  "indoor_temperatur" : "not_set_yet",
+#  "other_sensors" : "not_set_yes"
+#}
+#
+#
+#
+#
+#
+#
+#
+
+
 
 # Endpoint called when the button is clicked
 @app.route("/trigger", methods=["POST"])
@@ -31,13 +48,27 @@ def trigger():
 
 @app.route("/config", methods=["GET"])
 def readConfig():
-    return jsonify({"ausjelesen": "ausgelesen"})
+    content = "nothing"
+    with open(CONFIG_PATH, "r") as file:
+        print("Opened config file for reading")
+        content = file.read()
+
+    return jsonify({"ausjelesen": content})
 
 if __name__ == "__main__":
     try:
         with open(OPTIONS_PATH, "r") as f:
             options = json.load(f)
             SHYFT_ACCESS_KEY = options.get("shyft_access_key", SHYFT_ACCESS_KEY)
+        if not os.path.exists(CONFIG_PATH):
+            print("File does not exists")
+            with open(CONFIG_PATH, "w") as file:
+                file.write("created " + datetime.now())
+        else:
+            with open(CONFIG_PATH, "a") as file:
+                file.write("appended " + str(datetime.datetime.now()) +"\n")
+            print("Files does already exists")
+
     except Exception as e:
         print("Failed to load config from options.json:", e)
 
