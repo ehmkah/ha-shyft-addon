e53e96678f5faa202e251581460ed41f013664a4
Michael Krausse <michael.krausse@directbox.com>
2025-10-17 15:12:57 +0200

    display configuration and mock editabledle : read from hass_rest_api

--- shyft-addon/app.py
+++ shyft-addon/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, send_from_directory, request
+from flask import Flask, send_from_directory, jsonify, request
 import requests
 import json
 import datetime
@@ -10,6 +10,9 @@ SHYFT_ACCESS_KEY = "not_set_yet"
 OPTIONS_PATH = "/data/options.json"
 CONFIG_PATH = "/data/config.json"
 SUPERVISOR_TOKEN = value = os.getenv("SUPERVISOR_TOKEN")
+HASSIO_URI_RUNNING_ON_HAOS = "http://supervisor/core"
+HASSIO_URI_RUNNING_REMOTE = "http://homeassistant.local:8123"
+HASSIO_URI = HASSIO_URI_RUNNING_ON_HAOS
 
 # Serve the static HTML
 @app.route("/")
@@ -20,11 +23,13 @@ def index():
 @app.route("/trigger", methods=["POST"])
 def trigger():
     try:
+        valueForOne = loadEntityState("sensor.heatpump_mock_the_sensor_mock")
+
         headers = {
                 "Content-Type": "application/x-www-form-urlencoded",
                 "Authorization": f"Bearer {SHYFT_ACCESS_KEY}"
             }
-        payload = "aTestValue=created+by+TimeSeriesResultsEntityRepositoryIntegrationTest.+can+be+Removed+without+problems"
+        payload = "aTestValue=created+by+TimeSeriesResultsEntityRepositoryIntegrationTest.+can+be+Removed+without+problems&secondTestValue="+valueForOne
         external_url = "https://anselmhuewe.bubbleapps.io/version-test/api/1.1/obj/homeassistanttest"
 
         response = requests.post(external_url, headers=headers, data=payload)
@@ -32,6 +37,7 @@ def trigger():
          "external_status": response.status_code})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+
 
 @app.route("/config", methods=["GET"])
 def readConfig():
@@ -52,6 +58,17 @@ def writeConfig():
 
     return content
 
+def loadEntityState(sensorId):
+    headers = {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Authorization": f"Bearer {SUPERVISOR_TOKEN}"
+                }
+    completeUri = HASSIO_URI+"/api/states/"+sensorId
+    print("complete uri " +completeUri)
+    response =  requests.get(completeUri, headers=headers)
+    return response.json()["state"]
+
+
 if __name__ == "__main__":
     try:
         with open(OPTIONS_PATH, "r") as f:
@@ -61,7 +78,7 @@ if __name__ == "__main__":
             print("File does not exists")
             shutil.copy("www/defaultShyftConfig.json", CONFIG_PATH)
         else:
-            print("Files does already exists. nothing was copied")
+            print("File does already exists. nothing was copied")##
 
     except Exception as e:
         print("Failed to load config from options.json:", e)
