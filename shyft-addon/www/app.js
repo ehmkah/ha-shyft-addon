fb1053397f872d35114b2707c6dab2d792e732c9
Michael Krausse <michael.krausse@directbox.com>
2025-11-28 10:56:04 +0100

    KAN-82 : configure for more user firendly

--- shyft-addon/www/app.js
+++ shyft-addon/www/app.js
@@ -4,6 +4,57 @@ const configUri = insideHomeAssistant + "/config";
 const sensorIdsUri = insideHomeAssistant + "/sensorids";
 let configData = {}
 
+const helpinformation = {
+    "photovoltaic_powerflow_pv" : {
+        "label": "Aktueller Strom - Netz",
+        "description" :"Die aktuelle Leistung (in kW), die dein Haushalt aus dem öffentlichen Stromnetz bezieht bzw. dorthin einspeist."
+    }
+}
+
+/**
+    Aktueller Strom - Haushalt, Die Leistung (in kW), die dein Haushalt aktuell verbraucht.
+
+    Aktueller Strom - PV, Die aktuelle Leistung (in kW) deiner PV-Anlage.
+
+    Aktueller Strom - Netz, Die aktuelle Leistung (in kW), die dein Haushalt aus dem öffentlichen Stromnetz bezieht bzw. dorthin einspeist.
+
+    Aktueller Strom - Batterie, Die aktuelle Leistung (in kW), mit dem deine Batterie geladen (positiver Wert) oder entladen (negativer Wert) wird.
+
+    Ladestand, Aktueller Ladestand deiner Batterie in Prozent.
+
+    Batterie: Steuerungsmodus, Der Modus, in den deine Batterie versetzt werden kann (z.B. Eigenverbrauchsoptimierung, Netzladen)
+
+Batterie: Aktuelle max. Ladeleistung, Leistung (in kW), die an der Batterie als aktuelle Begrenzung für die Ladeleistung eingestellt ist.
+
+    Batterie: Max. Entladeleistung, Mit wieviel Leistung (in kW) darf deine Batterie höchstens entladen werden?
+
+    Temperatur Warmwassertank, Die aktuelle Temperatur im Warmwassertank (in °C)
+
+Warmwassermodus aktiviert? An/Aus, je nachdem ob an deiner Wärmepumpe die Warmwasserbereitung aktiviert ist oder nicht.
+
+    Warmwasser gerade erwärmt? An / Aus, je nachdem ob deine Wärmepumpe gerade Brauchwasser erwärmt oder nicht.
+
+Zieltemperatur (aktuell), Gewünschte Raumtemperatur in °C, die du an deiner Wärmepumpe einstellst. Über die stündliche Anpassung dieses Werte steuert Shyft die Leistung deiner Wärmepumpe.
+
+    Heizung aktiviert?, Ja / Nein, je nachdem, ob du die Heizung an deiner Wärmepumpe aktiviert hast oder nicht.
+
+    Aktuelle Leistung Wärmepumpe (elektrisch), in kW
+
+Wärmepumpe an/aus, An/Aus, je nachdem ob deine Wärmepumpe gerade läuft oder aus ist.
+
+    Innenraumtemperatur gemessen, Tatsächlich gemessene Innenraumtemperatur in °C. Für diesen Sensor musst du einen Temperatursensor mit Shyft verbinden.
+
+    Verbindungsstatus, Ja / Nein, je nachdem ob der Ladestecker deiner Wallbox im Auto eingesteckt ist oder nicht.
+
+    Auto - Ladestrom, Aktueller Ladestrom (in kW)
+
+EV - SOC, Ladestand deines Autos (in %)
+
+        ]
+
+}
+**/
+
 async function getJson(url) {
     const response = await fetch(url);
 
@@ -90,7 +141,9 @@ function renderSensorMappings(configData) {
     for (const [key, value] of Object.entries(configData)) {
         const row = document.createElement('tr');
         const keyCell = document.createElement('td');
-        keyCell.textContent = key;
+        const context = helpinformation[key] ?? {'label':key};
+        const label = context.label;
+        keyCell.textContent = label;
 
         const inputValue = document.createElement('input');
         inputValue.id = key + VALUE_POSTFIX;
