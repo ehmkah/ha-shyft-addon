8ebaef0c7795abc11b8ad9c6b69c663eedf06370
Michael Krausse <michael.krausse@directbox.com>
2025-12-17 16:09:34 +0100

    KAN-82 : shyft_adapter

---
+++ shyft-addon/shyft_adapter.py
@@ -0,0 +1,13 @@
+from constants import BUBBLE_URI
+
+# Communicates with shyft on bubble
+class ShyftAdapter:
+
+    def __init__(self):
+        self.bubble_uri = BUBBLE_URI
+
+    def send_pv_history(self, values):
+        pass
+
+
+
---
+++ shyft-addon/test_shyft_adapter.py
@@ -0,0 +1,7 @@
+from shyft_adapter import ShyftAdapter
+
+def test_send_pv_history():
+    sut = ShyftAdapter()
+
+
+
