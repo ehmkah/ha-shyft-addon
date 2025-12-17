6a7517dda8d019a22f00fa95336aa3fea5207688
Michael Krausse <michael.krausse@directbox.com>
2025-12-17 18:07:59 +0100

    KAN-82 : implemented with nice python modules

--- shyft-addon/test_sync_service.py
+++ shyft-addon/test_sync_service.py
@@ -2,12 +2,17 @@ from sync_service import SyncService
 from homeassistant_adapter import HomeAssistantAdapter
 from shyft_adapter import ShyftAdapter
 
+import pytest
 
-def test_trigger_pv_history():
-    sut = SyncService(HomeAssistantAdapter(), ShyftAdapter())
+@pytest.fixture
+def sut():
+    return SyncService(HomeAssistantAdapter(),
+                       ShyftAdapter(bubble_token="XXXX"),
+                       "shyft-addon/test_shyft_config.json")
 
+def test_trigger_pv_history(sut):
+    sut.sync_pv_history()
 
-def test_load_config():
-    sut = SyncService(HomeAssistantAdapter(), ShyftAdapter(), "shyft-addon/test_shyft_config.json")
+def test_load_config(sut):
     actual = sut._load_config()
     assert actual["sensorMappings"]["photovoltaic_powerflow_pv"] == "sensor.heatpump_mock_the_sensor_mock"
